46a38d89f06f11d54fbaee4d0f63ac17b2d0f360
svill <villella.sam@gmail.com>
2019-12-18 12:25:07 +1100

    Stage 1 of the tutorial

---
+++ src/_rot13_test.js
@@ -0,0 +1,17 @@
+// Copyright Titanium I.T. LLC.
+"use strict";
+
+const assert = require("./assert.js");
+const rot13 = require("./rot13.js");
+
+describe("Rot13", function() {
+
+	it("doesn’t do anything with an empty string", function() {
+		assert.equal(rot13.transform(""), "");
+	});
+
+	it("doesn’t encode non-alphabetical characters", function() {
+		assert.equal(rot13.transform("!"), "!");
+	});
+
+});
