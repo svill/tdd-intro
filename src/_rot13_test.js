259528892e19b797ff6fa6731d3bc736edac9f8c
svill <villella.sam@gmail.com>
2019-12-18 12:33:53 +1100

    Stage 2 of the tutorial

--- src/_rot13_test.js
+++ src/_rot13_test.js
@@ -6,12 +6,20 @@ const rot13 = require("./rot13.js");
 
 describe("Rot13", function() {
 
-	it("doesn’t do anything with an empty string", function() {
+	it("doesn't do anything with an empty string", function() {
 		assert.equal(rot13.transform(""), "");
 	});
 
-	it("doesn’t encode non-alphabetical characters", function() {
-		assert.equal(rot13.transform("!"), "!");
+	it("doesn't encode non-alphabetical characters", function() {
+		assert.equal(rot13.transform("!@#$%^"), "!@#$%^");
+	});
+
+	it("encodes lower-case characters", function() {
+		assert.equal(rot13.transform("abcdefghijklmnopqrstuvwxyz"), "nopqrstuvwxyzabcdefghijklm");
+	});
+	
+	it("encodes upper-case characters", function() {
+		assert.equal(rot13.transform("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "NOPQRSTUVWXYZABCDEFGHIJKLM");
 	});
 
 });
--- src/rot13.js
+++ src/rot13.js
@@ -1,5 +1,40 @@
 "use strict";
 
 exports.transform = function(input) {
-  return input;
+  let str = "";
+  for (var i = 0; i < input.length; i++) {
+    str += transformChar(input.charAt(i));
+  }
+  return str;
 };
+
+function transformChar(char) {
+  const code = codeFor(char);
+  if (code >= codeFor("a") && code <= codeFor("m")) {
+    return transformLeft(char);
+  }
+  if (code >= codeFor("n") && code <= codeFor("z")) {
+    return transformRight(char);
+  }
+  if (code >= codeFor("A") && code <= codeFor("M")) {
+    return transformLeft(char);
+  }
+  if (code >= codeFor("N") && code <= codeFor("Z")) {
+    return transformRight(char);
+  }
+  return char;
+}
+
+function transformLeft(char) {
+  const newCode = codeFor(char) + 13;
+  return String.fromCharCode(newCode);
+}
+
+function transformRight(char) {
+  const newCode = codeFor(char) - 13;
+  return String.fromCharCode(newCode);
+}
+
+function codeFor(char) {
+  return char.charCodeAt(0);
+}
